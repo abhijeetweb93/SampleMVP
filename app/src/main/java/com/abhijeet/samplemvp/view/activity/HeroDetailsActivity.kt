c805b3a8a9e2c7b2f9a682fb982c571052ad8633
asharma <asharma@avaloninfosys.com>
2019-09-23 15:46:17 +0530

    Complete MVP Sample App.

--- app/src/main/java/com/abhijeet/samplemvp/view/activity/HeroDetailsActivity.kt
+++ app/src/main/java/com/abhijeet/samplemvp/view/activity/HeroDetailsActivity.kt
@@ -1,8 +1,6 @@
 package com.abhijeet.samplemvp.view.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import com.abhijeet.samplemvp.R
 import com.abhijeet.samplemvp.base.BaseActivity
 import com.abhijeet.samplemvp.base.BaseModel
@@ -10,21 +8,32 @@ import com.abhijeet.samplemvp.base.BasePresenter
 import com.abhijeet.samplemvp.base.IBaseView
 import com.abhijeet.samplemvp.data_model.Hero
 import com.abhijeet.samplemvp.databinding.ActivityHeroDetailsBinding
+import com.bumptech.glide.Glide
 
 
-class HeroDetailsActivity :AppCompatActivity() {
+class HeroDetailsActivity : BaseActivity<BasePresenter<IBaseView, BaseModel>>() {
+    override fun initPresenter(): BasePresenter<IBaseView, BaseModel> {
+        return BasePresenter()
+    }
 
-
-
-    var binding:ActivityHeroDetailsBinding?=null
+    var binding: ActivityHeroDetailsBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_hero_details)
-//        setContentView(R.layout.activity_hero_details)
+        binding = putContentView(R.layout.activity_hero_details)
 
         if (getIntent().hasExtra("hero")) {
             val hero = getIntent().extras?.get("hero") as Hero
+            binding?.hero = hero
+
+            binding?.ivProfile?.let {
+                Glide
+                    .with(this)
+                    .load(hero.imageurl)
+                    .centerCrop()
+                    .placeholder(android.R.drawable.ic_btn_speak_now)
+                    .into(it)
+            }
         }
     }
 }
